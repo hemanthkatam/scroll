22bba467b3fa89b349afe1cc418e4b404cafd349
Hemanthkatam <katamkv@gmail.com>
2025-02-23 13:47:29 +0530

    bars

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
+
+const ProgressBar = ({ id, onCompleteProgress }) => {
+  const [status, setStatus] = useState(0);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setStatus((prevStatus) => prevStatus + 1); // Use previous state
+    }, 100);
+
+    return () => clearInterval(interval); // Cleanup on unmount
+  }, []);
+
+  useEffect(() => {
+    if (status >= 100) {
+      onCompleteProgress(id);
+    }
+  }, [status]);
+
+  return <progress value={status} max={100} className="progressBar"></progress>;
+};
 
 function App() {
+  const [barsCount, setBarCount] = useState([]);
+
+  const onClick = () => {
+    const random = Date.now() % 1000;
+
+    setBarCount([...barsCount, { statue: "NOT_STARTED", id: random }]);
+  };
+
+  const onCompleteProgress = (id) => {
+    setBarCount(barsCount.filter((count) => count.id !== id));
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => setBarCount([])}>Reset progress bar</button>
+      <button onClick={onClick}>Create progress bar</button>
+      <div>
+        qued progess bars: {barsCount.length - 5 > 0 ? barsCount.length - 5 : 0}
+      </div>
+
+      {barsCount.slice(0, 5).map((count) => (
+        <ProgressBar
+          key={count.id}
+          id={count.id}
+          onCompleteProgress={onCompleteProgress}
+        />
+      ))}
     </div>
   );
 }
